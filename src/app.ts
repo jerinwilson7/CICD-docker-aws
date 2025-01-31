e95ef8099b7ceb4275646803aae37159fe220ec3
JerinWilson <jerin.wilson@vonnue.com>
2025-01-31 14:00:05 +0530

    feat: test deployment

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express, { Express, Request, Response } from "express";
 const app: Express = express();
 
 app.get("/", (req: Request, res: Response) => {
-  res.status(200).json("Hello from the server!!");
+  res.status(200).json("DEPLOYMENT SUCCESS (:");
 });
 
 app.listen(8080, () => {
