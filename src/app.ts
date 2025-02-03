d6fce4a8d384571075d9d756506a7071b9ff94b7
JerinWilson <jerin.wilson@vonnue.com>
2025-02-03 10:24:50 +0530

    feat: env test deployment -3

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,14 @@ import express, { Express, Request, Response } from "express";
 
 dotenv.config()
 
-console.log("PORT",process.env.PORT)
+const port = process.env.PORT
+
 const app: Express = express();
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json("DEPLOYMENT SUCCESS test 7  (:");
 });
 
-app.listen(8080, () => {
-  console.log(`App is listening on port 8080`);
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
 });
